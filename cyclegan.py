3aaff0375e35392d4d3e09a3dd4328fb06fbf70b
skt0725 <skt0725@kaist.ac.kr>
2022-08-16 17:18:14 +0900

    lr scheduler

--- cyclegan.py
+++ cyclegan.py
@@ -1,5 +1,6 @@
 import glob
 import os
+from sys import path_hooks
 from time import time
 
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 import itertools
+from skimage.metrics import peak_signal_noise_ratio as psnr
 from easydict import EasyDict as edict
 from scipy import io
 from torch.utils.data import DataLoader, Dataset
@@ -24,45 +26,68 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 opt = edict()
 opt.resume = False
 opt.ckpt_dir = "./checkpoint"
-opt.resume = False
 opt.ckpt_reload = 0
-opt.data_dir = "./data/Low_High_CT_mat_slice"
+opt.train_dir = "./data/Low_High_CT_mat_slice"
+opt.test_dir = "./data/test"
 opt.result_save_dir = "./result"
+opt.test_save_dir = "./test"
 opt.lr = 0.0002
-opt.epoch = 150
+opt.epoch = 200
 opt.batch_size = 16
 opt.img_size = 128
 opt.loss_lambda = 10
 
 # data load & preprocess
-img_path = glob.glob(os.path.join(opt.data_dir, '**/*.mat'), recursive=True)
+train_path = glob.glob(os.path.join(opt.train_dir, '**/*.mat'), recursive=True)
+test_path = glob.glob(os.path.join(opt.test_dir, '**/*.mat'), recursive=True)
 low = []
 high = []
-for path in img_path:
+for path in train_path:
     img_mat = io.loadmat(path)
     img_low_high = img_mat.get('imdb')
     low.append(img_low_high[0][0][0])
     high.append(img_low_high[0][0][1])
+test_low = []
+test_high = []
+for path in test_path:
+    test_mat = io.loadmat(path)
+    test_low_high = test_mat.get('imdb')
+    test_low.append(test_low_high[0][0][0])
+    test_high.append(test_low_high[0][0][1])
+
 class AAPM(Dataset):
-    def __init__(self, transform = None):
+    def __init__(self, train, transform = None):
         self.len = len(low)
         self.low = low
         self.high = high
+        self.test_low = test_low
+        self.test_high = test_high
         self.transform = transform
+        self.train = train
 
     def __len__(self):
         return self.len
     
     def __getitem__(self, idx):
         if self.transform is not None:
-            low = self.transform(self.low[idx])
-            high = self.transform(self.high[idx])
-            low_min, low_max = low.min(), low.max()
-            high_min, high_max = high.min(), high.max()
-            # min-max scale
-            low = (low-low_min)/(low_max-low_min)
-            high = (high-high_min)/(high_max-high_min)
-            return low, high
+            if self.train:
+                low = self.transform(self.low[idx])
+                high = self.transform(self.high[idx])
+                low_min, low_max = low.min(), low.max()
+                high_min, high_max = high.min(), high.max()
+                # min-max scale
+                low = (low-low_min)/(low_max-low_min)
+                high = (high-high_min)/(high_max-high_min)
+                return low, high
+            else:
+                low = self.transform(self.test_low[idx])
+                high = self.transform(self.test_high[idx])
+                low_min, low_max = low.min(), low.max()
+                high_min, high_max = high.min(), high.max()
+                # min-max scale
+                low = (low-low_min)/(low_max-low_min)
+                high = (high-high_min)/(high_max-high_min)
+                return low, high
         else:
             return self.low, self.high
 
@@ -72,10 +97,11 @@ transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-dataset = AAPM(transform = transform)
-dataloader = DataLoader(dataset=dataset, batch_size = opt.batch_size, shuffle=True, num_workers = 1, drop_last = True)
-
-# low, high = next(iter(dataloader))
+train_dataset = AAPM(train = True, transform = transform)
+train_data = DataLoader(dataset=train_dataset, batch_size = opt.batch_size, shuffle=True, num_workers = 1, drop_last = True)
+test_dataset = AAPM(train = False, transform = transform)
+test_data = DataLoader(dataset=test_dataset, batch_size = opt.batch_size, drop_last = True, shuffle = True, num_workers = 1)
+# low, high = next(iter(train_data))
 # print(low.shape)
 # fig, ax = plt.subplots(1, 2)
 # ax[0].imshow(low.squeeze(), cmap="gray")
@@ -173,6 +199,9 @@ class Discriminator(nn.Module):
             nn.init.normal(m.weight.data, 1, 0.02)
             nn.init.constant(m.bias.data, 0)
 
+def lr_decay(epoch):
+    lr = 1 - max(0, epoch - 100)/200
+    return lr
 
 G_low_high = Generator().to(device)
 G_high_low = Generator().to(device)
@@ -181,6 +210,9 @@ D_high = Discriminator().to(device)
 gen_optimizer = torch.optim.Adam(itertools.chain(G_low_high.parameters(),G_high_low.parameters()), lr=opt.lr)
 dis_low_optimizer = torch.optim.Adam(D_low.parameters(), lr=opt.lr)
 dis_high_optimizer = torch.optim.Adam(D_high.parameters(), lr=opt.lr)
+lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(gen_optimizer, lr_lambda=lr_decay)
+lr_scheduler_D_low = torch.optim.lr_scheduler(dis_low_optimizer, lr_lambda=lr_decay)
+lr_scheduler_D_high = torch.optim.lr_scheduler(dis_high_optimizer, lr_lambda=lr_decay)
 summary = SummaryWriter('logs/')
 epoch = 0
 
@@ -200,12 +232,17 @@ if opt.resume:
         print(e)
 ones = torch.ones((opt.batch_size, 1)).to(device)
 zeros = torch.zeros((opt.batch_size, 1)).to(device)
+best_accuracy = 0
 criterion_identity = nn.L1Loss()
 criterion_gan = nn.MSELoss()
 criterion_cycle = nn.L1Loss()
 for epoch in range(epoch, opt.n_epochs):
     start_time = time()
-    for i, (low, high) in enumerate(dataloader):
+    G_low_high.train()
+    G_high_low.train()
+    D_low.train()
+    D_high.train()
+    for i, (low, high) in enumerate(train_data):
         low = low.to(device)
         high = high.to(device)
         # train generator
@@ -247,6 +284,7 @@ for epoch in range(epoch, opt.n_epochs):
         loss_D_high = (high_fake_loss + high_real_loss)/2
         loss_D_high.backward()
         dis_high_optimizer.step()
+        accuracy = psnr(high, fake_high)
 
     if not os.path.exists(opt.result_save_dir):
         os.makedirs(opt.result_save_dir)
@@ -264,14 +302,71 @@ for epoch in range(epoch, opt.n_epochs):
         fake_low = fake_low.view(opt.batch_size, 1, 256,256)
         save_image(fake_high, os.path.join(opt.result_save_dir, f"{epoch}_high.png"), nrow=4)
         save_image(fake_low, os.path.join(opt.result_save_dir, f"{epoch}_low.png"), nrow=4)
-
-    torch.save(dict(epoch = epoch, G_low_high = G_low_high.state_dict(), G_high_low = G_high_low.state_dict(), D_low = D_low.state_dict(), D_low = D_low.state_dict(), D_high = D_high.state_dict(), 
-                gen_optimizer = gen_optimizer.state_dict(), dis_low_optimizer = dis_low_optimizer.state_dict(), dis_high_optimizer = dis_high_optimizer.state_dict()), str(opt.ckpt_dir)+'/'+str(epoch)+'.pt')
     t = time()-start_time
-    print(f'Epoch {epoch}/{opt.n_epochs} || discriminator low loss={loss_D_low:.4f} || discriminator high loss={loss_D_high:.4f} || generator loss={loss_G:.4f} || time {t:.3f}')
+    lr_scheduler_G.step()
+    lr_scheduler_D_low.step()
+    lr_scheduler_D_high.step()
+    print(f'Train: Epoch {epoch}/{opt.n_epochs} || discriminator low loss={loss_D_low:.4f} || discriminator high loss={loss_D_high:.4f} || generator loss={loss_G:.4f} || accuracy = {accuracy:.3f} || time {t:.3f}')
     summary.add_scalar("dis_low", loss_D_low, epoch)
     summary.add_scalar("dis_high", loss_D_high, epoch)
     summary.add_scalar("gen", loss_G, epoch)
+
+    G_low_high.eval()
+    G_high_low.eval()
+    D_low.eval()
+    D_high.eval()
+    start_time = time()
+    with torch.no_grad():
+        for i, (test_low, test_high) in enumerate(test_data):
+            low = test_low.to(device)
+            high = test_high.to(device)
+            fake_low = G_high_low(high)
+            fake_high = G_low_high(low)
+            # identity loss
+            loss_identity_low = criterion_identity(G_high_low(low),low)
+            loss_identity_high = criterion_identity(G_low_high(high), high)
+            # GAN loss 
+            fake_high = G_low_high(low)
+            fake_low = G_high_low(high)
+            loss_gan_low = criterion_gan(D_high(fake_high), ones)
+            loss_gan_high = criterion_gan(D_low(fake_low), ones)
+            # cycle loss
+            loss_cycle_low = criterion_cycle(G_high_low(fake_high), low)
+            loss_cycle_high = criterion_cycle(G_low_high(fake_low), high)
+            loss_G = loss_identity_high + loss_identity_low + loss_gan_high + loss_gan_low + loss_cycle_high + loss_cycle_low
+            # discriminator_low
+            fake_low_output = D_low(fake_high)
+            low_fake_loss = criterion_gan(fake_low_output, zeros)
+
+            real_low_output = D_low(low)
+            low_real_loss = criterion_gan(real_low_output, ones)
+
+            loss_D_low = (low_fake_loss + low_real_loss)/2
+            # discriminator_high
+            dis_high_optimizer.zero_grad()
+            fake_high_output = D_high(fake_low)
+            high_fake_loss = criterion_gan(fake_high_output, zeros)
+
+            real_high_output = D_high(high)
+            high_real_loss = criterion_gan(real_high_output, ones)
+
+            loss_D_high = (high_fake_loss + high_real_loss)/2  
+            accuracy = psnr(high, fake_high)  
+            if best_accuracy < accuracy:
+                best_accuracy = accuracy
+                fake_high = fake_high.view(opt.batch_size, 1, 256,256)
+                high = high.view(opt.batch_size, 1, 256,256)
+                save_image(high, "./best_high.png", nrow=4)
+                save_image(fake_high, "./best_fake_high", nrow=4)
+    if (epoch+1) % 5 == 0:
+        fake_high = fake_high.view(opt.batch_size, 1, 256,256)
+        high = high.view(opt.batch_size, 1, 256,256)
+        save_image(high, os.path.join(opt.test_save_dir, f"{epoch}_high.png"), nrow=4)
+        save_image(fake_high, os.path.join(opt.test_save_dir, f"{epoch}_fake_high.png"), nrow=4)
+    time = time() - start_time
+    print(f'Test : Epoch {epoch}/{opt.n_epochs} || discriminator low loss={loss_D_low:.4f} || discriminator high loss={loss_D_high:.4f} || generator loss={loss_G:.4f} || accuracy = {accuracy:.3f} || time {t:.3f}')        
+    torch.save(dict(epoch = epoch, G_low_high = G_low_high.state_dict(), G_high_low = G_high_low.state_dict(), D_low = D_low.state_dict(), D_low = D_low.state_dict(), D_high = D_high.state_dict(), 
+                gen_optimizer = gen_optimizer.state_dict(), dis_low_optimizer = dis_low_optimizer.state_dict(), dis_high_optimizer = dis_high_optimizer.state_dict()), str(opt.ckpt_dir)+'/'+str(epoch)+'.pt')
 summary.close()
 
 
